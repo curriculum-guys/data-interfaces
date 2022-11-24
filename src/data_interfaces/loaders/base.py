4495566d52bbdb1c86eed57388509b442b014bb2
Ventura <arthur.plautz@indicium.tech>
2022-11-24 10:53:38 -0300

    Add Data Format

--- src/data_interfaces/loaders/base.py
+++ src/data_interfaces/loaders/base.py
@@ -7,6 +7,7 @@ class BaseLoader:
         self.seed = seed
         self.experiment = experiment
         self.trials = trials
+        self.set_data_format()
 
     @property
     def __root_dir(self):
@@ -16,6 +17,16 @@ class BaseLoader:
     def max_gen(self):
         return max(self.data.index)
 
+    def set_data_format(self,  suffix='run', interface=None, date=None):
+        base_name = f's{self.seed}'
+        if not interface and not date:
+            return base_name + f'_{suffix}.csv'
+        if interface:
+            base_name += f'_{interface}'
+        if date:
+            base_name += f'_d{date}'
+        return base_name + '.csv'
+
     def get_data(self, subpath, **kwargs):
-        data_path = f'{self.data_dir}/{subpath}/s{self.seed}_run.csv'
+        data_path = f'{self.data_dir}/{subpath}/{self.data_format}'
         return pd.read_csv(data_path, index_col='gen', **kwargs)
